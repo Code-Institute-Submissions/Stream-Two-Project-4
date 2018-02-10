15c43d77e0236f62db058601c52417b72811262c
andreweliyah <andreweliyah@icloud.com>
2018-02-10 10:30:28 -0800

    Updated the about page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,6 +13,10 @@ def stat():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/img/<filename>")
+def img(filename):
+  return send_from_directory('img', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
